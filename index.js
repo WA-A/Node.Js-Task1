4ece1731eb381e2f3229dfd0da1a6fef58fa5ceb
wasan <wasanawwad2020@gmail.com>
2024-01-17 20:40:15 +0200

    Api with array

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')  // Import express
 const app = express();
 
-console.log(app);
+// Api with array 
+const users =[
+    {
+        name:"Wasan",
+        email:"wasanawwwad2020@gmail.com",
+        password:"123",
+        age:21
 
+    },
+    {
+        name:"Ansam",
+        email:"ansamawwwad1996@gmail.com",
+        password:"1234",
+        age:27
 
+    }
+
+]
+
+console.log(users);
